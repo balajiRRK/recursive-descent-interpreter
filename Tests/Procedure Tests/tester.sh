27584ee9a50cb01743effce396a4cfdad1e8dc5f
balajiRRK <rbalajirrk@gmail.com>
2025-03-30 22:42:02 -0400

    WIP of github actions tester

--- Tests/Procedure Tests/tester.sh
+++ Tests/Procedure Tests/tester.sh
@@ -7,78 +7,56 @@ javac -d . ../../*.java
 javac Main.java
 runner="java -cp . Main"
 
-for value in {1..11}
+# Correct test cases
+count=$(find "Correct/" -maxdepth 1 -type f -regex  ".*/[0-9]+\.code" | wc -l)
+for ((i = 1; i <= $count; i++))
 do
 	echo ""
-	echo "Running ${value}.code"
-	timeout 5 ${runner} Correct/${value}.code Correct/${value}.data > Correct/${value}.student
-	echo "Running diff with ${value}.expected"
-	grep -o '[[:digit:]]\+' Correct/${value}.student > Correct/temp1
-	grep -o '[[:digit:]]\+' Correct/${value}.expected > Correct/temp2
+	echo "Running ${i}.code"
+	timeout 5 ${runner} Correct/${i}.code Correct/${i}.data > Correct/${i}.student
+	echo "Running diff with ${i}.expected"
+	grep -o '[[:digit:]]\+' Correct/${i}.student > Correct/temp1
+	grep -o '[[:digit:]]\+' Correct/${i}.expected > Correct/temp2
 	if cmp -s "Correct/temp1" "Correct/temp2"; then
 		echo "Print looks good"
-		score=$(($score + 1))
+		correctScore=$(($correctScore + 1))
 	else
 		echo "Student output and expected output are different"
 	fi
 done
+echo ""
 
 rm Correct/temp1
 rm Correct/temp2
 
-echo ""
+echo "----------------"
 echo "Running error cases:"
+echo "----------------"
+
+count=$(find "Error/" -maxdepth 1 -type f -regex  ".*/[0-9]+\.code" | wc -l)
+for ((i = 1; i <= $count; i++))
+do
+	echo ""
+	echo "Running ${i}.code"
+	timeout 5 ${runner} Error/${i}.code Error/${i}.data > Error/${i}.student
+	echo "Running diff with ${i}.expected"
+	grep -o '[[:digit:]]\+' Error/${i}.student > Error/temp1
+	grep -o '[[:digit:]]\+' Error/${i}.expected > Error/temp2
+	if cmp -s "Error/temp1" "Error/temp2"; then
+		echo "Print looks good"
+		errorScore=$(($errorScore + 1))
+	else
+		echo "Student output and expected output are different"
+	fi
+done
 echo ""
 
-echo "Running 00.error:"
-echo "-----Student Output Starts-----"
-timeout 5 ${runner} Error/00.code Error/00.data
-echo "-----Student Output Ends-----"
-read -n 1 -p "Error procedure body missing (no stmt-seq). Error message related to that? (y/n)" mainmenuinput
-if [ $mainmenuinput = "y" ]; then
-	error=$(($error + 1))
-fi
-echo ""
-echo ""
-
-echo "Running 01.error:"
-echo "-----Student Output Starts-----"
-timeout 5 ${runner} Error/01.code Error/01.data
-echo "-----Student Output Ends-----"
-read -n 1 -p "Error calling a procedure that has not been declared. Error message related to that? (y/n)" mainmenuinput
-if [ $mainmenuinput = "y" ]; then
-	error=$(($error + 1))
-fi
-echo ""
-echo ""
-
-echo "Running 02.error:"
-echo "-----Student Output Starts-----"
-timeout 5 ${runner} Error/02.code Error/02.data
-echo "-----Student Output Ends-----"
-read -n 1 -p "Error is duplicate formal parameters. Error message related to that? (y/n)" mainmenuinput
-if [ $mainmenuinput = "y" ]; then
-	error=$(($error + 1))
-fi
-echo ""
-echo ""
-
-echo "Running 03.error:"
-echo "-----Student Output Starts-----"
-timeout 5 ${runner} Error/03.code Error/03.data
-echo "-----Student Output Ends-----"
-read -n 1 -p "Semantic error, duplicate procedure name. Error message related to that? (y/n)" mainmenuinput
-if [ $mainmenuinput = "y" ]; then
-	error=$(($error + 1))
-fi
-echo ""
-echo ""
-
-
+rm Error/temp1
+rm Error/temp2
 
 echo "Correct cases score out of 11:"
-echo $score
+echo $correctScore
 echo "Error cases score out of 4:"
-echo $error
+echo $errorScore
 
 echo "Done!"
