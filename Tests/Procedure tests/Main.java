5b3014028bdacb470fd4f8b37fda72d9663c5dfd
Balaji Radhakrishnan <rbalajirrk@gmail.com>
2025-04-04 19:49:52 -0400

    fixed case of Procedure Tests

---
+++ Tests/Procedure tests/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(args[0]);
+        Parser.scanner = s;
+
+        Procedure p = new Procedure();
+        p.parse();
+
+        Memory.s = new Scanner(args[1]);
+        p.execute();
+    }
+}
---
+++ Tests/Procedure tests/tester.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "Attempting to compile java code..."
+javac -d . ../../*.java
+javac Main.java
+runner="java -cp . Main"
+
+# Correct test cases
+correctCount=$(find "Correct/" -maxdepth 1 -type f -regex  ".*/[0-9]+\.code" | wc -l)
+for ((i = 1; i <= $correctCount; i++))
+do
+	echo ""
+	echo "Running ${i}.code"
+	timeout 5 ${runner} Correct/${i}.code Correct/${i}.data > Correct/${i}.student
+	echo "Running diff with ${i}.expected"
+	grep -o '[[:digit:]]\+' Correct/${i}.student > Correct/temp1
+	grep -o '[[:digit:]]\+' Correct/${i}.expected > Correct/temp2
+	if cmp -s "Correct/temp1" "Correct/temp2"; then
+		echo "Print looks good"
+		correctScore=$(($correctScore + 1))
+	else
+		echo "Student output and expected output are different"
+	fi
+done
+echo ""
+
+rm Correct/temp1
+rm Correct/temp2
+
+echo "----------------"
+echo "Running error cases:"
+echo "----------------"
+
+errorCount=$(find "Error/" -maxdepth 1 -type f -regex  ".*/[0-9]+\.code" | wc -l)
+for ((i = 1; i <= $errorCount; i++))
+do
+	echo ""
+	echo "Running ${i}.code"
+	timeout 5 ${runner} Error/${i}.code Error/${i}.data > Error/${i}.student
+	echo "Running diff with ${i}.expected"
+	grep -o '[[:digit:]]\+' Error/${i}.student > Error/temp1
+	grep -o '[[:digit:]]\+' Error/${i}.expected > Error/temp2
+	if cmp -s "Error/temp1" "Error/temp2"; then
+		echo "Print looks good"
+		errorScore=$(($errorScore + 1))
+	else
+		echo "Student output and expected output are different"
+	fi
+done
+echo ""
+
+rm Error/temp1
+rm Error/temp2
+
+echo "Correct cases score out of $correctCount:"
+echo $correctScore
+echo "Error cases score out of $errorCount:"
+echo $errorScore
+
+echo "Done!"
